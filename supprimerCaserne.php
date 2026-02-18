48e04a63f2fb47817d5e59484e85062ead38cab8
MD200613 <Dumais.Maxym@gmail.com>
2026-02-18 13:42:12 -0500

    suppression des casernes fontionnel

---
+++ supprimerCaserne.php
@@ -0,0 +1,7 @@
+<?php
+    $pdo = new PDO("mysql:host=localhost;dbname=pompier","root","");
+    $ins = $pdo->prepare("delete from caserne where id=:id;");
+    $ins->execute([':id' => $_POST['id']]);
+    echo $_POST['id'];
+    header('Location: index.php');
+?>
